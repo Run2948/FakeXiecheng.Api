5af7e2640a50315767534f129f9b9ff4aeab5feb
zhujinrun <15997352948@163.com>
2020-12-04 09:01:17 +0800

    添加第一个API

--- FakeXiecheng.Api/Startup.cs
+++ FakeXiecheng.Api/Startup.cs
@@ -16,6 +16,7 @@ namespace FakeXiecheng.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,11 @@ namespace FakeXiecheng.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
+                endpoints.MapControllers();
             });
         }
     }
